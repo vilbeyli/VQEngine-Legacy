af25e45f546edf1ba56b3d0ade0ca1a5566a9890
Volkan Ilbeyli <volkanilbeyli@gmail.com>
2018-05-01 00:32:16 -0700

    fix build script cherry pick

--- BUILD.bat
+++ BUILD.bat
@@ -73,7 +73,8 @@ EXIT /B 0
 
 REM Cleanup before build for a clean build
 :CleanUp
-echo Robocopy   : Cleaning up the project folders...
+echo Cleaning up the project folders...
+if not exist Build mkdir Build
 cd Build
 if not exist Empty mkdir Empty
 robocopy ./Empty ./ /purge /MT:8 > nul
@@ -92,7 +93,7 @@ rmdir Empty
 cd ..
 set root_dir=%cd%
 REM get all .dll and .exe files from build
-for /r %proj_dir% %%f in (*.exe *.dll) do (
+for /r "%proj_dir%" %%f in (*.exe *.dll) do (
     REM skip _artifacts folder itself
     if not "%%~df%%~pf" == "%artifacts_dir%\" ( 
         xcopy "%%f" "%artifacts_dir%" > nul
@@ -108,7 +109,7 @@ EXIT /B 0
 if %1==True (echo MSBuild    : Building the project [Multi-thread]...) else (echo MSBuild    : Building the project [Single-thread]...)
 
 REM cleanup log file if it exists
-if exist %2 rm %2 
+if exist %2 del %2 
 
 set t_msb_begin=%TIME%
 echo              begin %t_msb_begin%
@@ -122,7 +123,7 @@ EXIT /B 0
 :Devenv_Build
 
 REM cleanup log file if it exists
-if exist %2 rm %2 
+if exist %2 del %2 
 
 echo devenv     : Building the project...
 echo              begin %TIME%
